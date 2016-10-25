af0b05bd9192a3e1f6141322de5bb56c34e67b55
X7RMN7R <uv0.xtr@gmail.com>
2016-10-25 10:59:52 +0200

    querydsl maven config

--- src/main/java/hello/CustomerRepository.java
+++ src/main/java/hello/CustomerRepository.java
@@ -2,9 +2,10 @@ package hello;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long>{
+public interface CustomerRepository extends JpaRepository<Customer, Long>, QueryDslPredicateExecutor<Customer>{
 
 	List<Customer> findByLastName(String lastName);
 }
